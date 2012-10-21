c601884ecde0024a80e9e4575849be2bee14aaae
oulaya <lea.nassih@gmail.com>
2012-10-21 19:08:14 +0200

    configurer le format des commentaire YAML

--- src/fichier/FileParser.java
+++ src/fichier/FileParser.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class FileParser {
 
@@ -12,9 +13,19 @@ public class FileParser {
 	 * @param args
 	 */
 	
-	public void parse(File file, IContentHandler handler){
+	public void parse(File file,String format, IContentHandler handler){
 		
 		try{
+			Scanner f=new Scanner (new File(format));
+			while(f.hasNext()){
+				String l=f.nextLine();
+				if (l.contains("comment"))
+				{
+					format=l.substring(l.indexOf("\"")+1,l.indexOf("\"",l.indexOf("\"")+1));
+					
+			break;
+			}
+			}
 			InputStream ips=new FileInputStream(file); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
@@ -22,7 +33,7 @@ public class FileParser {
 			while ((ligne=br.readLine())!=null){
 				handler.defaultLine(ligne);
 			
-				if (ligne.startsWith("#")){
+				if (ligne.startsWith(format)){
 					
 					handler.commentLine(ligne);
 					
